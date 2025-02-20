a0a83f270a2fa853513239f10499946c024dcc4a
Sumit Joshi <106461510+ssjoshi89@users.noreply.github.com>
2025-02-20 11:44:19 +0530

    Add files via upload

--- menu.php
+++ menu.php
@@ -1,165 +1,149 @@
-<?php
-	$page = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
-?>
-
-<aside class="app-side" id="app-side">
-					<div class="side-content ">
-						
-						<nav class="side-nav">
-							<ul class="unifyMenu" id="unifyMenu">
+<!-- main header -->
+            <div class="nk-header is-light">
+                <div class="container-fluid">
+                    <div class="nk-header-wrap">
+                        <div class="nk-menu-trigger me-sm-2 d-lg-none">
+                            <a href="#" class="nk-nav-toggle nk-quick-nav-icon" data-target="headerNav"><em class="icon ni ni-menu"></em></a>
+                        </div>
+                        <div class="nk-header-brand">
+                            <a href="index.php" class="logo-link">
+                                <img class="logo-light logo-img" src="../../img/hsbc.png" srcset="../../img/hsbc.png 2x" width="50px" alt="logo">
+								<img class="logo-dark logo-img" src="../../img/hsbc.png" srcset="../../img/hsbc.png" width="50px" alt="logo-dark">
 								
-								<li class="menu-header">
-									<p align="center"> ----- Menu Controls ----- </p>
-								</li>
+                            </a>
+                        </div><!-- .nk-header-brand -->
+                        <div class="nk-header-menu ms-auto" data-content="headerNav">
+                            <div class="nk-header-mobile">
+                                <div class="nk-header-brand">
+                                    <a href="index.php" class="logo-link">
+                                        <img class="logo-light logo-img" src="../../img/hsbc.png" srcset="../../img/hsbc.png 2x" width="50px" alt="logo">
+                                        <img class="logo-dark logo-img" src="../../img/hsbc.png" srcset="../../img/hsbc.png 2x" width="50px" alt="logo-dark">
+                                    </a>
+                                </div>
+                                <div class="nk-menu-trigger me-n2">
+                                    <a href="#" class="nk-nav-toggle nk-quick-nav-icon" data-target="headerNav"><em class="icon ni ni-arrow-left"></em></a>
+                                </div>
+                            </div>
+                            <ul class="nk-menu nk-menu-main ui-s2">
+                                <li class="nk-menu-item has-sub">
+                                    <a href="index.php" class="nk-menu-link">
+                                        <span class="nk-menu-text">Dashboard</span>
+                                    </a>
+                                </li><!-- .nk-menu-item -->
+                                
+								<li class="nk-menu-item has-sub">
+                                    <a href="onboardings.php" class="nk-menu-link">
+                                        <span class="nk-menu-text">Onboardings</span>
+                                    </a>
+                                </li><!-- .nk-menu-item -->
 								
-								<?php
-									$data=mysqli_fetch_row(mysqli_query($con,"select distinct group_concat(name) from env_master where status='Active' and find_in_set(id,'".$_SESSION['env']."') order by id"));
-									$data=explode(',',$data[0]);	
-
-									$api_flag=mysqli_fetch_row(mysqli_query($con,"select api_flag from project_info where id='".$_SESSION['id']."'"));
-								?>	
-
-								<li class="<?php if($page=="index") { echo "active selected"; } ?>">
-									<a href="index.php">
-										<span class="has-icon">
-											<i class="icon-gauge"></i>
-										</span>
-										<span class="nav-title">Dashboard</span>
-									</a>
-								</li>	
-
-								
-								<li class="<?php foreach($data as $rw){ if($page=="chaos_".$rw."") { echo "active selected"; }} ?>">
-									<a href="#" class="has-arrow" aria-expanded="false">
-										<span class="has-icon">
-											<i class="icon-cogs"></i>
-										</span>
-										<span class="nav-title">Chaos </span>
-									</a>
-									<ul aria-expanded="false">
-									<?php
-										foreach($data as $rw)
-										{
-											if($page=="chaos_".$rw."")
-											{
-												echo "<li><a href='chaos_".$rw.".php' class='current-page'>".$rw."</a></li>";
-											}
-											else
-											{
-												echo "<li><a href='chaos_".$rw.".php'>".$rw."</a></li>";
-											}
-										}			
-									?>	
-									</ul>
-								</li>
-								
-								<?php
-									if($api_flag[0]=="Yes")
-									{
-								?>
-								
-								<li class="<?php if($page=="api") { echo "active selected"; } ?>">
-									<a href="api.php">
-										<span class="has-icon">
-											<i class="icon-swap"></i>
-										</span>
-										<span class="nav-title">API</span>
-									</a>
-								</li>
-								
-								<?php
-									}
-								?>
-								
-								<li class="<?php if($page=="notes") { echo "active selected"; } ?>">
-									<a href="notes.php">
-										<span class="has-icon">
-											<i class="icon-new-message"></i>
-										</span>
-										<span class="nav-title">Chaos Diary</span>
-									</a>
-								</li>
-								
-								<?php
-									if($_SESSION['owner']==$_SESSION['usr'])
-									{
-								?>
-								
-								<li class="<?php foreach($data as $rw){ if($page=="list_".$rw."" || $page=="conf_".$rw."" || $page=="conf_".$rw."Updt") { echo "active selected"; }} ?>">
-									<a href="#" class="has-arrow" aria-expanded="false">
-										<span class="has-icon">
-											<i class="icon-tools"></i>
-										</span>
-										<span class="nav-title">Configurations</span>
-									</a>
-									<ul aria-expanded="false">
-									<?php
-										foreach($data as $rw)
-										{
-											if($page=="list_".$rw."" || $page=="conf_".$rw."" || $page=="conf_".$rw."Updt")
-											{
-												echo "<li><a href='list_".$rw.".php' class='current-page'>".$rw."</a></li>";
-											}
-											else
-											{
-												echo "<li><a href='list_".$rw.".php'>".$rw."</a></li>";
-											}
-										}			
-									?>
-									</ul>
-								</li>
-								
-								<li class="<?php if($page=="projectProfile" || $page=="userProfile") { echo "active selected"; } ?>">
-									<a href="#" class="has-arrow" aria-expanded="false">
-										<span class="has-icon">
-											<i class="icon-center_focus_strong"></i>
-										</span>
-										<span class="nav-title">Project Profile</span>
-									</a>
-									<ul aria-expanded="false">
-									<?php
-										if($page=="projectProfile")
-										{
-											echo "<li><a href='projectProfile.php' class='current-page'>Project Management</a></li>";
-										}
-										else
-										{
-											echo "<li><a href='projectProfile.php'>Project Management</a></li>";
-										}	
-
-										if($page=="userProfile")
-										{
-											echo "<li><a href='userProfile.php' class='current-page'>User Management</a></li>";
-										}
-										else
-										{
-											echo "<li><a href='userProfile.php'>User Management</a></li>";
-										}	
-									?>
-									</ul>
-								</li>
-								
-								<?php
-									}
-								?>
-								
-								
-								<li class="menu-header">
-									<hr />
-								</li>
-								
-								<li>
-									<a href="signout.php">
-										<span class="has-icon">
-											<i class="icon-switch"></i>
-										</span>
-										<span class="nav-title">Signout</span>
-									</a>
-								</li>
-							</ul>
 							
-						</nav>
-						
-					</div>
-					
-				</aside>
+								<!--<li class="nk-menu-item has-sub">
+                                    <a href="#" class="nk-menu-link nk-menu-toggle">
+                                        <span class="nk-menu-text">Projects</span>
+                                    </a>
+                                    <ul class="nk-menu-sub">
+                                        
+									<?php
+										/*if($_SESSION['role']=="1")
+										{
+											echo '
+												<li class="nk-menu-item">
+													<a href="html/_blank.html" class="nk-menu-link"><span class="nk-menu-text">Admin Support</span></a>
+												</li>
+											';
+										}
+										
+										if($_SESSION['role']=="2")
+										{
+											echo '
+												<li class="nk-menu-item">
+													<a href="html/_blank.html" class="nk-menu-link"><span class="nk-menu-text">Branch Support</span></a>
+												</li>
+											';
+										}*/
+									?>
+									
+                                        <li class="nk-menu-item">
+                                            <a href="html/pages/terms-policy.html" class="nk-menu-link"><span class="nk-menu-text">Technical Support</span></a>
+                                        </li>
+                                    </ul><!-- .nk-menu-sub -->
+                               <!-- </li> --> <!-- .nk-menu-item -->
+								
+								<li class="nk-menu-item has-sub">
+                                    <a href="#" class="nk-menu-link nk-menu-toggle">
+                                        <span class="nk-menu-text">Reports</span>
+                                    </a>
+                                    <ul class="nk-menu-sub">
+                                        <li class="nk-menu-item">
+                                            <a href="teamExecutionRpt.php" class="nk-menu-link"><span class="nk-menu-text">Team Execution Report</span></a>
+                                        </li>
+										<li class="nk-menu-item">
+                                            <a href="serviceRpt.php" class="nk-menu-link"><span class="nk-menu-text">Execution Report (Service Wise)</span></a>
+                                        </li>
+										<li class="nk-menu-item">
+                                            <a href="experimentRpt.php" class="nk-menu-link"><span class="nk-menu-text">Execution Report (Experiment Wise)</span></a>
+                                        </li>
+										<li class="nk-menu-item">
+                                            <a href="targetRpt.php" class="nk-menu-link"><span class="nk-menu-text">Execution Report (Target Wise)</span></a>
+                                        </li>
+                                    </ul><!-- .nk-menu-sub -->
+                                </li><!-- .nk-menu-item -->
+								
+                            </ul><!-- .nk-menu -->
+                        </div><!-- .nk-header-menu -->
+                        <div class="nk-header-tools">
+                            <ul class="nk-quick-nav">
+                                <li class="dropdown user-dropdown">
+                                    <a href="#" class="dropdown-toggle" data-bs-toggle="dropdown">
+                                        <div class="user-toggle">
+                                            <div class="user-avatar sm">
+                                                <em class="icon ni ni-user-alt"></em>
+                                            </div>
+                                        </div>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-md dropdown-menu-end dropdown-menu-s1 is-light">
+                                        <div class="dropdown-inner user-card-wrap bg-lighter d-none d-md-block">
+                                            <div class="user-card">
+                                                <div class="user-avatar bg-danger">
+                                                    <span>
+													<?php 
+														$words = explode(" ", $_SESSION['name']);
+														$acronym = "";
+														foreach ($words as $w) {
+														  $acronym .= mb_substr($w, 0, 1);
+														}
+														echo $acronym;
+													?>
+													</span>
+                                                </div>
+                                                <div class="user-info">
+                                                    <span class="lead-text"><?php echo $_SESSION['name']; ?></span>
+                                                    <span class="sub-text">
+														<?php 
+															echo "Portal Administrator"; 
+														?>
+													</span>
+                                                </div>
+                                            </div>
+                                        </div>
+										
+										
+                                        <div class="dropdown-inner">
+                                            <ul class="link-list">
+                                                <li><a href="#" data-bs-toggle="modal" data-bs-target="#chgpwd"><em class="icon ni ni-setting-alt"></em><span>Change Passcode</span></a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="dropdown-inner">
+                                            <ul class="link-list">
+                                                <li><a href="signout.php"><em class="icon ni ni-signout"></em><span>Sign out</span></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li><!-- .dropdown -->
+                            </ul><!-- .nk-quick-nav -->
+                        </div><!-- .nk-header-tools -->
+                    </div><!-- .nk-header-wrap -->
+                </div><!-- .container-fliud -->
+            </div>
+            <!-- main header -->
---
+++ serviceRpt.php
@@ -0,0 +1,155 @@
+<?php include("header.php"); ?>
+
+<body class="nk-body bg-lighter ">
+    <div class="nk-app-root">
+        <!-- wrap @s -->
+        <div class="nk-wrap ">
+            
+			<?php include("menu.php"); ?>
+			
+            <!-- content @s -->
+            <div class="nk-content ">
+                <div class="container-fluid">
+                    <div class="nk-content-inner">
+                        <div class="nk-content-body">
+                            <div class="components-preview mx-auto">
+                                
+                                <div class="nk-block nk-block-lg">
+                                    <div class="nk-block-head">
+                                        <div class="nk-block-head-content">
+                                            <h4 class="nk-block-title">Execution Report</h4>
+                                            <div class="nk-block-des">
+                                                <p>Project Team Execution Report (Service Wise)</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+									
+									<div class="card card-bordered">
+                                        <div class="card-inner">
+                                            <div class="card-head">
+                                                <h5 class="card-title">Report Information</h5>
+                                            </div>
+                                            <form method="post">
+                                                <div class="row g-4">
+                                                    
+													<div class="col-lg-6">
+                                                        <div class="form-group">
+                                                            <label class="form-label" for="pay-amount-1">Select Team Name <font color="red">*</font></label>
+                                                            <div class="form-control-wrap">
+                                                                <select class="form-select js-select2" data-search="on" name="team">
+																	<?php
+																		$data_city=mysqli_query($con,"select id,name,team_name from project_info where (status!='Requested' and status!='Cancelled')");
+																		while($rw=mysqli_fetch_row($data_city))
+																		{
+																			if($_POST['team']==$rw[0])
+																			{
+																				echo "<option value='".$rw[0]."' selected>".$rw[0]." | ".$rw[1]." - ".$rw[2]."</option>";
+																			}
+																			else
+																			{
+																				echo "<option value='".$rw[0]."'>".$rw[0]." | ".$rw[1]." - ".$rw[2]."</option>";
+																			}
+																		}														
+																	?>														
+																</select>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+													
+													
+                                                    <div class="col-12">
+                                                        <div class="form-group">
+                                                            <input type="submit" class="btn btn-lg btn-primary" name="searchResults" value="Search Results">
+                                                        </div>
+                                                    </div>
+                                                </div>
+												
+                                            </form>
+                                        </div>
+                                    </div>
+									
+								<?php
+									if(isset($_POST['searchResults']))
+									{
+								?>
+									
+									
+									<div class="card card-bordered card-preview">
+                                        <div class="card-inner">
+                                            <table class="datatable-init-export wrap table table-bordered" data-export-title="Export">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Experiment Name</th>
+														<th>Environments</th>
+														<th>Target Name</th>
+														<th>Service Name</th>
+														<th>Run Count</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    
+													<?php
+														$data_row=mysqli_query($con,"SELECT chaos_name,env_name,
+
+CASE
+    WHEN env_name='GCP-GKE' THEN (SELECT concat(cluster,' | ',namespace) FROM gcp_gke_config where id=chaos_log.target_name)
+    WHEN env_name='GCP-VM' THEN (SELECT concat(mgmt_vm_name,' | ',mgmt_vm_type) FROM gcp_vm_config where id=chaos_log.target_name)
+	WHEN env_name='IKP' THEN (SELECT concat(cluster,' | ',namespace) FROM ikp_config where id=chaos_log.target_name)
+	WHEN env_name='On-Premise' THEN (SELECT concat(mgmt_vm_name,' | ',mgmt_vm_type) FROM on_prem_config where id=chaos_log.target_name)
+END as 'target',rmk,count(*) as 'count'
+
+ FROM chaos_log
+where project_id='".$_POST['team']."' 
+group by rmk
+order by env_name,id");
+														while($rw=mysqli_fetch_row($data_row))
+														{
+															$arr=explode('|',$rw[3]);
+															
+															if(is_numeric($arr[0]))
+															{
+																$arr[0]="";
+															}
+															
+															echo '
+																<tr>
+																	<td>'.$rw[0].'</td>
+																	<td>'.$rw[1].'</td>
+																	<td>'.$rw[2].'</td>
+																	<td>'.$arr[0].'</td>
+																	<td>'.$rw[4].'</td>																	
+																</tr>
+																';																
+														}
+													?>
+													
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div><!-- .card-preview -->
+									
+								<?php
+									}
+								?>	
+									
+                                </div> <!-- nk-block -->
+                                
+                            </div><!-- .components-preview wide-lg mx-auto -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- content @e -->
+            
+			<?php include("credits.php"); ?>
+			
+        </div>
+        <!-- wrap @e -->
+    </div>
+    <!-- app-root @e -->
+	
+    <?php include("footer.php"); ?>
+</body>
+
+</html>
