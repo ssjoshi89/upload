972cfbdf6f6c9b64f96cd1fa68b2cf1b4da21fab
Sumit Joshi <106461510+ssjoshi89@users.noreply.github.com>
2025-02-26 18:30:44 +0530

    Add files via upload

--- serviceRpt.php
+++ serviceRpt.php
@@ -25,93 +25,73 @@
                                     </div>
                                     
 									
-									<div class="card card-bordered">
-                                        <div class="card-inner">
-                                            <div class="card-head">
-                                                <h5 class="card-title">Report Information</h5>
-                                            </div>
-                                            <form method="post">
-                                                <div class="row g-4">
-                                                    
-													<div class="col-lg-6">
-                                                        <div class="form-group">
-                                                            <label class="form-label" for="pay-amount-1">Select Team Name <font color="red">*</font></label>
-                                                            <div class="form-control-wrap">
-                                                                <select class="form-select js-select2" data-search="on" name="team">
-																	<?php
-																		$data_city=mysqli_query($con,"select id,name,team_name from project_info where (status!='Requested' and status!='Cancelled')");
-																		while($rw=mysqli_fetch_row($data_city))
-																		{
-																			if($_POST['team']==$rw[0])
-																			{
-																				echo "<option value='".$rw[0]."' selected>".$rw[0]." | ".$rw[1]." - ".$rw[2]."</option>";
-																			}
-																			else
-																			{
-																				echo "<option value='".$rw[0]."'>".$rw[0]." | ".$rw[1]." - ".$rw[2]."</option>";
-																			}
-																		}														
-																	?>														
-																</select>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-													
-													
-                                                    <div class="col-12">
-                                                        <div class="form-group">
-                                                            <input type="submit" class="btn btn-lg btn-primary" name="searchResults" value="Search Results">
-                                                        </div>
-                                                    </div>
-                                                </div>
-												
-                                            </form>
-                                        </div>
-                                    </div>
-									
-								<?php
-									if(isset($_POST['searchResults']))
-									{
-								?>
-									
-									
 									<div class="card card-bordered card-preview">
                                         <div class="card-inner">
                                             <table class="datatable-init-export wrap table table-bordered" data-export-title="Export">
                                                 <thead>
                                                     <tr>
-                                                        <th>Service Name</th>
-														<th>Environments</th>
+                                                        <th>Project Name</th>
+														<th>SPOC</th>
+														<th>Service Name</th>
 														<th>Run Count</th>
+														<th>Total Services</th>
                                                     </tr>
                                                 </thead>
                                                 <tbody>
                                                     
 													<?php
-														$data_row=mysqli_query($con,"SELECT distinct rmk,env_name,count(*) as 'count'
-
- FROM chaos_log
-where project_id='".$_POST['team']."' 
-group by rmk
-order by env_name,id");
-														while($rw=mysqli_fetch_row($data_row))
+														$serviceCount=0;
+														
+														$data_project=mysqli_query($con,"SELECT distinct project_id,name,project_owner FROM chaos_log join project_info on project_info.id=chaos_log.project_id");
+														while($rw_prj=mysqli_fetch_row($data_project))
 														{
-															$arr=explode('|',$rw[0]);
+															mysqli_next_result($con);		
+															$serviceCount=0;
+															//$data_row=mysqli_query($con,"SELECT distinct SUBSTRING_INDEX(rmk, '|', 1) FROM chaos_log where ((SUBSTRING_INDEX(rmk, '|', 1) REGEXP '^[0-9]+$') !=1) and project_id='".$rw_prj[0]."'");
 															
-															if(is_numeric($arr[0]))
-															{
-																$arr[0]="";
+															$data_row=mysqli_query($con,"SELECT distinct SUBSTRING_INDEX(rmk, '|', 1),count(*) FROM chaos_log where ((SUBSTRING_INDEX(rmk, '|', 1) REGEXP '^[0-9]+$') !=1) and project_id='".$rw_prj[0]."' group by SUBSTRING_INDEX(rmk, '|', 1) order by id");
+															while($rw=mysqli_fetch_row($data_row))
+															{																																
+																if($rw[0]!="")
+																{
+																	$serviceCount=$serviceCount+1;
+																}
 															}
-															else
-															{														
-																echo '
-																	<tr>
-																		<td>'.$arr[0].'</td>
-																		<td>'.$rw[1].'</td>
-																		<td>'.$rw[2].'</td>																																
-																	</tr>
-																	';																
+															
+															$data_row=mysqli_query($con,"SELECT distinct SUBSTRING_INDEX(rmk, '|', 1),count(*) FROM chaos_log where ((SUBSTRING_INDEX(rmk, '|', 1) REGEXP '^[0-9]+$') !=1) and project_id='".$rw_prj[0]."' group by SUBSTRING_INDEX(rmk, '|', 1) order by id");
+															while($rw=mysqli_fetch_row($data_row))
+															{																																
+																if($rw[0]!="")
+																{
+																	echo '<tr><td>'.$rw_prj[1].'</td><td>'.$rw_prj[2].'</td><td>'.$rw[0].'</td><td>'.$rw[1].'</td><td>'.$serviceCount.'</td></tr>';
+																}
 															}
+															
+															/*while($rw=mysqli_fetch_row($data_row))
+															{													
+																if($rw[0]!="")
+																{
+																	$serviceCount=$serviceCount+1;
+																}
+															}
+															$i=0;*/
+															//echo '<script>alert('.$serviceCount.');</script>';
+															/*$data_row=mysqli_query($con,"SELECT distinct SUBSTRING_INDEX(rmk, '|', 1) FROM chaos_log where ((SUBSTRING_INDEX(rmk, '|', 1) REGEXP '^[0-9]+$') !=1) and project_id='".$rw_prj[0]."'");
+															while($rw=mysqli_fetch_row($data_row))
+															{																																
+																if($rw[0]!="")
+																{
+																	if($i==0)
+																	{
+																		echo '<tr><td rowspan="'.$serviceCount.'">'.$rw_prj[1].'</td><td rowspan="'.$serviceCount.'">'.$rw_prj[2].'</td><td>'.$rw[0].'</td><td rowspan="'.$serviceCount.'">'.$serviceCount.'</td></tr>';
+																	}
+																	else
+																	{
+																		echo '<tr><td>'.$rw[0].'</td></tr>';
+																	}
+																	$i=$i+1;																																	
+																}
+															}*/
 														}
 													?>
 													
@@ -119,11 +99,7 @@ order by env_name,id");
                                             </table>
                                         </div>
                                     </div><!-- .card-preview -->
-									
-								<?php
-									}
-								?>	
-									
+								
                                 </div> <!-- nk-block -->
                                 
                             </div><!-- .components-preview wide-lg mx-auto -->
